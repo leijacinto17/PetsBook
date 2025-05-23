0eae2851d9dac13591348e7b9c0b42fd715bd609
Mark Leo Jacinto Koala <mjacinto.koala@telusinternational.com>
2025-05-24 00:43:31 +0800

    Add posts controller and base api controller

---
+++ Backend/PetsBook.API/Common/BaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsBook.API.Common
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseApiController<T> : ControllerBase
+    {
+        protected readonly ILogger<T> _logger;
+
+        protected BaseApiController(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        // You can put shared logic, services, etc. here
+
+        protected void LogInfo(string message) => _logger.LogInformation(message);
+
+        protected void LogError(Exception ex) => _logger.LogError(ex, ex.Message);
+    }
+}
---
+++ Backend/PetsBook.API/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetsBook.API.Common;
+
+namespace PetsBook.API.Controllers
+{
+    public class PostController : BaseApiController<PostController>
+    {
+        public PostController(ILogger<PostController> logger) : base(logger) { }
+
+        [HttpGet]
+        public IActionResult TestLogging()
+        {
+            try
+            {
+                LogInfo("TestLogging endpoint was called.");
+
+                // Simulate some logic
+                var posts = new[] { "Post 1", "Post 2", "Post 3" };
+
+                return Ok(new
+                {
+                    Message = "Logging test successful",
+                    Data = posts
+                });
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                return StatusCode(500, "Something went wrong");
+            }
+        }
+    }
+}
