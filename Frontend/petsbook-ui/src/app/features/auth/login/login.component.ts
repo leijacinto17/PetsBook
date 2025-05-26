81f8a5a5b246799eab2f0206c79523ff045c1efb
drix <sirdrix_enrico@yahoo.com>
2025-05-26 19:13:29 +0800

    initial commit for frontend

---
+++ Frontend/petsbook-ui/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Implement your login logic here
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // Add authentication logic and navigate to the next page upon successful login
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PetsBook
 A social media platform for all fur parents
+
+
+# Backend
+
+.NET Core 9 Web API
+
+# Frontend
+
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.13.
